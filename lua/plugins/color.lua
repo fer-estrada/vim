0ced698674c25e82759013ebb5997fda6254e163
fer-estrada <fernandodestrada@gmail.com>
2025-08-17 11:05:13 +0200

    changed colorscheme (again), added list to color.lua, removed render-md

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,8 +1,14 @@
 return {
-    'ajmwagar/vim-deus',
+    'AlessandroYorba/Alduin',
     priority = 1000,
     config = function()
         vim.o.background = 'dark'
-        vim.cmd[[colo deus]]
+        vim.cmd[[colo alduin]]
     end,
 }
+
+-- ajmwagar/vim-deus
+-- ellisonleao/gruvbox.nvim
+-- andreasvc/vim-256noir
+-- projekt0n/caret.nvim
+-- marko-cerovac/material.nvim
