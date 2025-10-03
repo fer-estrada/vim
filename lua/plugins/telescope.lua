88f991eddd193c469131ff1a708634bc80c091f6
fer-estrada <fernandodestrada@gmail.com>
2025-10-03 10:36:02 +0200

    ripgrep dependency for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    dependecies = { 'nvim-lua/plenary.nvim' },
+    dependecies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' },
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set("n", "<leader>pf", builtin.find_files)
