2fd69c8dd13ab7a062273e91b309519774a3cca5
fer-estrada <fernandodestrada@gmail.com>
2025-10-05 21:50:28 +0200

    removed devicons dependency from alpha.lua
    added mini.pairs plugin

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,6 +1,6 @@
 return {
     'goolord/alpha-nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function ()
         local alpha = require("alpha")
         local dashboard = require("alpha.themes.dashboard")
