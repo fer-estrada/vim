7890ffb3edb0d095997893b05379393f496c2952
fer-estrada <fernandodestrada@gmail.com>
2026-07-24 22:11:51 +0200

    switching treesitter to main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,5 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    branch = 'master',
     lazy = false,
     build = ':TSUpdate',
-    opts = {
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust", "java" },
-        auto_install = true,
-        highlight = {
-            enable = false,
-            additional_vim_regex_highlighting = false,
-        },
-    },
 }
