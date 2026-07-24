2d9a622e5047b8d4a26dc15e6e3118b3756279e8
fer-estrada <fernandodestrada@gmail.com>
2026-07-25 01:43:57 +0200

    comment of keybinds for comment plugin

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -2,3 +2,14 @@ return {
     'numToStr/Comment.nvim',
     opts = {},
 }
+
+-- keybinds
+-- gcc -> line into comment
+-- gbc -> block into comment
+-- gc - gb when in visual mode
+-- other uses are: [count]gcc, gc[count][direction]
+-- works for gbc and gb too
+
+-- gcw -> word into comment
+-- gco - gcO -> insert new line below/above into comment
+-- gcA -> insert at the end of line into comment
