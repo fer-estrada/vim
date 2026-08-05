215c4561c0a7c24f33b593a267958d84188e24d9
fer-estrada <fernandodestrada@gmail.com>
2026-08-05 03:50:33 +0200

    added commit keybind for fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,5 +2,6 @@ return {
     'tpope/vim-fugitive',
     config = function()
         vim.keymap.set("n", "<leader>Ga", "<cmd>Git add .<CR>")
+        vim.keymap.set("n", "<leader>Gm", "<cmd>Git commit<CR>")
     end,
 }
