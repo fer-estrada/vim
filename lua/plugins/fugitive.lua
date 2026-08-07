a370f11c970a47fb4ee117a1d7d933d6086f7ddd
fer-estrada <fernandodestrada@gmail.com>
2026-08-07 14:40:21 +0200

    git status keybind for fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,5 +3,6 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>Ga", "<cmd>Git add .<CR>")
         vim.keymap.set("n", "<leader>Gm", "<cmd>Git commit<CR>")
+        vim.keymap.set("n", "<leader>Gs", "<cmd>Git status<CR>")
     end,
 }
