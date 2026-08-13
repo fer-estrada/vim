49a8403a2480b1a3250618c9738877d1cd8dd40c
fer-estrada <fernandodestrada@gmail.com>
2026-08-13 14:40:58 +0200

    reset

--- lua/plugins/fugitive.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'tpope/vim-fugitive',
-    config = function()
-        vim.keymap.set("n", "<leader>Ga", "<cmd>Git add .<CR>")
-        vim.keymap.set("n", "<leader>Gm", "<cmd>Git commit<CR>")
-        vim.keymap.set("n", "<leader>Gs", "<cmd>Git status<CR>")
-    end,
-}
