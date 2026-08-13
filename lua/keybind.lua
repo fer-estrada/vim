6be3e79ccef8e9b79969bc6b1af5579eb2e785a5
fer-estrada <fernandodestrada@gmail.com>
2026-08-13 20:19:09 +0200

    alpha plugin

--- lua/keybind.lua
+++ lua/keybind.lua
@@ -10,6 +10,9 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
 -- nvim config keybinds
 vim.keymap.set("n", "<leader><leader>s", vim.cmd.source)
 vim.keymap.set("n", "<leader><leader>r", vim.cmd.restart)
