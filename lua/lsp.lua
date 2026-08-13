7e713f6f4dcea26949bd7638f84938f8c3d1c7b8
fer-estrada <fernandodestrada@gmail.com>
2026-08-13 15:45:10 +0200

    all lsp files added

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -2,4 +2,4 @@ vim.diagnostic.config({
     virtual_text = true,
 })
 
-vim.lsp.enable({ "lua_ls", "clangd" })
+vim.lsp.enable({ "lua_ls", "clangd", "asm_lsp", "ts_ls", "rust_analyzer", "gopls", "vls" })
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,5 +2,8 @@ return {
     {
         "nvim-mini/mini.pairs",
         version = false,
+        config = function()
+            require("mini.pairs").setup()
+        end,
     }
 }
